c426adddaea41db103bed2754fe8777c8b6e6cae
zfchen <zfchen@cs.hku.hk>
2019-06-10 15:46:18 +0800

    new loss

--- fun/lossPackage.py
+++ fun/lossPackage.py
@@ -15,7 +15,7 @@ class HLoss(nn.Module):
         return b
 
 class lossEvaluator(nn.Module):
-    def __init__(self, margin=0.1, biLossFlag=True, lossWFlag=False, lamda=0.8, struct_flag=False, struct_only_flag=False, entropy_regu_flag=False, lamda2=0.1):
+    def __init__(self, margin=0.1, biLossFlag=True, lossWFlag=False, lamda=0.8, struct_flag=False, struct_only_flag=False, entropy_regu_flag=False, lamda2=0.1, loss_type=''):
         super(lossEvaluator, self).__init__()
         self.margin =margin
         self.biLossFlag = biLossFlag
@@ -25,17 +25,146 @@ class lossEvaluator(nn.Module):
         self.struct_only_flag = struct_only_flag
         self.entropy_regu_flag = entropy_regu_flag
         self.lamda2 = lamda2
+        self.loss_type = loss_type 
         if self.entropy_regu_flag:
             self.entropy_calculator = HLoss()
 
-    def forward(self, imFtr=None, disFtr=None, lblList=None, simMM=None):
+    def forward(self, imFtr=None, disFtr=None, lblList=None, simMM=None, region_gt_ori=None):
         if self.lossWFlag:
             loss = self.forwardRankW_v2(imFtr, disFtr, lblList)
             return loss
+        if self.wsMode=='coAtt' and  self.loss_type=='triplet_full':
+            loss  = self.forward_fully_supervised(simMM, lblList, region_gt_ori)
+            #loss  = self.forward_fully_supervised_v2(simMM, lblList, region_gt_ori)
+            return loss
         if self.wsMode=='rankTube' or self.wsMode=='rankFrm':
             loss = self.forwardRank(imFtr, disFtr, lblList)
-        elif self.wsMode=='coAtt' or self.wsMode =='coAttV2' or self.wsMode=='coAttV3' or self.wsMode=='coAttV4':
-            loss = self.forwardCoAtt(simMM, lblList)
+        elif self.wsMode=='coAtt' or self.wsMode =='coAttV2' or self.wsMode=='coAttV3' or self.wsMode=='coAttV4' or self.wsMode=='coAttBi':
+            #loss = self.forwardCoAtt(simMM, lblList)
+            loss = self.forwardCoAtt_efficient(simMM, lblList)
+            #print(loss-loss_e)
+            #print(loss)
+            #print(loss_e)
+        return loss
+
+
+    def forward_fully_supervised_v2(self, simMMRe, lblList, region_gt_ori, thre=0.5):
+        #pdb.set_trace()
+        bSize = len(lblList)
+        if(len(lblList)==1):
+            return torch.zeros(1).cuda()
+        loss = torch.zeros(1).cuda()
+        pairNum = 0.0000001
+
+        mean_pos_list = list()
+        for i in range(bSize):
+            region_gt_ori_i = torch.from_numpy(region_gt_ori[i]).cuda()
+            tmp_mask_pos = region_gt_ori_i>thre
+            tmp_mask_neg = region_gt_ori_i<0.2
+                #loss +=tmp_neg_val 
+        loss = loss /pairNum
+        if pairNum <1:
+            if tmp_mask_pos.sum() >0:
+                tmp_pos_val = torch.min( torch.masked_select(simMMRe[i, :, i], tmp_mask_pos))
+                loss -= tmp_pos_val
+            elif tmp_mask_neg.sum()>0:
+                tmp_neg_val = torch.max(torch.masked_select(simMMRe[i, :, i], tmp_mask_neg))
+                loss += tmp_neg_val 
+            else:
+                tmp_val = torch.mean(simMMRe[i, :, i])
+                loss += tmp_val
+
+        t1 = time.time()
+        print(pairNum)
+        if self.entropy_regu_flag:
+            #simMMRe = simMM.view(bSize, -1, bSize)
+            # simMMRe: bSize, prpNum, bSize
+            simMMRe = simMMRe.squeeze()
+            ftr_match_pair_list = list()
+            ftr_unmatch_pair_list = list()
+
+            for i in range(bSize):
+                for j in range(bSize):
+                    if i==j:
+                        ftr_match_pair_list.append(simMMRe[i, ..., i])
+                    elif lblList[i]!=lblList[j]:
+                        ftr_unmatch_pair_list.append(simMMRe[i, ..., j])
+
+            ftr_match_pair_mat = torch.stack(ftr_match_pair_list, 0)
+            ftr_unmatch_pair_mat = torch.stack(ftr_unmatch_pair_list, 0)
+            match_num = len(ftr_match_pair_list)
+            unmatch_num = len(ftr_unmatch_pair_list)
+            if match_num>0:
+                entro_loss = self.entropy_calculator(ftr_match_pair_mat)
+            loss +=self.lamda2*entro_loss 
+            print('entropy loss: %3f ' %(float(entro_loss)))
+        else:
+            loss +=2
+            #pdb.set_trace()
+        print('\n')
+        return loss
+
+    def forward_fully_supervised(self, simMMRe, lblList, region_gt_ori, thre=0.5):
+        #pdb.set_trace()
+        bSize = len(lblList)
+        if(len(lblList)==1):
+            return torch.zeros(1).cuda()
+        loss = torch.zeros(1).cuda()
+        pairNum = 0.0000001
+
+        mean_pos_list = list()
+        for i in range(bSize):
+            region_gt_ori_i = torch.from_numpy(region_gt_ori[i]).cuda()
+            tmp_mask_pos = region_gt_ori_i>0.4
+            tmp_mask_neg = region_gt_ori_i<0.2
+            if tmp_mask_pos.sum() >0 and tmp_mask_neg.sum()>0:
+                tmp_pos_val = torch.min( torch.masked_select(simMMRe[i, :, i].squeeze(), tmp_mask_pos))
+                tmp_neg_val = torch.max(torch.masked_select(simMMRe[i, :, i].squeeze(), tmp_mask_neg))
+                tmp_loss = tmp_neg_val - tmp_pos_val + self.margin
+                if tmp_loss >0:
+                    loss += tmp_loss 
+
+                pairNum +=1
+            #else:
+                #tmp_neg_val = torch.max(torch.masked_select(simMMRe[i, :, i], tmp_mask_neg))
+                #loss +=tmp_neg_val 
+        loss = loss /pairNum
+        if pairNum <1:
+            if tmp_mask_pos.sum() >0:
+                tmp_pos_val = torch.min( torch.masked_select(simMMRe[i, :, i], tmp_mask_pos))
+                loss -= tmp_pos_val
+            elif tmp_mask_neg.sum()>0:
+                tmp_neg_val = torch.max(torch.masked_select(simMMRe[i, :, i], tmp_mask_neg))
+                loss += tmp_neg_val 
+            else:
+                tmp_val = torch.mean(simMMRe[i, :, i])
+                loss += tmp_val 
+        t1 = time.time()
+        print(pairNum)
+        if self.entropy_regu_flag:
+            #simMMRe = simMM.view(bSize, -1, bSize)
+            # simMMRe: bSize, prpNum, bSize
+            simMMRe = simMMRe.squeeze()
+            ftr_match_pair_list = list()
+            ftr_unmatch_pair_list = list()
+
+            for i in range(bSize):
+                for j in range(bSize):
+                    if i==j:
+                        ftr_match_pair_list.append(simMMRe[i, ..., i])
+                    elif lblList[i]!=lblList[j]:
+                        ftr_unmatch_pair_list.append(simMMRe[i, ..., j])
+
+            ftr_match_pair_mat = torch.stack(ftr_match_pair_list, 0)
+            ftr_unmatch_pair_mat = torch.stack(ftr_unmatch_pair_list, 0)
+            match_num = len(ftr_match_pair_list)
+            unmatch_num = len(ftr_unmatch_pair_list)
+            if match_num>0:
+                entro_loss = self.entropy_calculator(ftr_match_pair_mat)
+            loss +=self.lamda2*entro_loss 
+            print('entropy loss: %3f ' %(float(entro_loss)))
+            #pdb.set_trace()
+        print('\n')
         return loss
 
     def forwardRank(self, imFtr, disFtr, lblList):
@@ -50,7 +179,6 @@ class lossEvaluator(nn.Module):
         loss = torch.zeros(1).cuda()
         pairNum = 0.000001
 
-#        pdb.set_trace()
         t1 = time.time()
         #print(lblList)
         if not self.struct_only_flag:
@@ -123,6 +251,71 @@ class lossEvaluator(nn.Module):
         print('\n')
         return loss
 
+    def forwardCoAtt_efficient(self, simMMRe, lblList):
+        
+        #pdb.set_trace()
+        simMax, maxIdx= torch.max(simMMRe.squeeze(3), dim=1)
+        loss = torch.zeros(1).cuda()
+        pair_num = 0.000001
+
+        t1 = time.time()
+        #print(lblList)
+        bSize = len(lblList)
+        b_size = simMax.shape[0]
+        pos_diag = torch.cat([simMax[i, i].unsqueeze(0) for i in range(b_size)])
+        one_mat = torch.ones(b_size, b_size).cuda()
+        pos_diag_mat = torch.mul(pos_diag, one_mat)
+        pos_diag_trs = pos_diag_mat.transpose(0,1)
+
+        mask_val = torch.ones(b_size, b_size).cuda()
+
+        for i in range(b_size):
+            lblI = lblList[i]
+            for j in range(i, b_size):
+                lblJ = lblList[j]
+                if lblI==lblJ:
+                    mask_val[i, j]=0
+                    mask_val[j, i]=0
+        pair_num = pair_num + torch.sum(mask_val)
+        
+        loss_mat_1 = simMax -pos_diag + self.margin 
+        loss_mask = (loss_mat_1>0).float()
+        loss_mat_1_mask = loss_mat_1 *loss_mask * mask_val 
+        loss1 = torch.sum(loss_mat_1_mask)
+        
+        loss_mat_2 = simMax -pos_diag_trs +self.margin
+        loss_mask_2 = (loss_mat_2>0).float()
+        loss_mat_2_mask = loss_mat_2 *loss_mask_2 * mask_val 
+        loss2 = torch.sum(loss_mat_2_mask)
+        loss =  (loss1+loss2)/pair_num
+
+        if self.entropy_regu_flag:
+            #pdb.set_trace()
+            #simMMRe = simMM.view(bSize, -1, bSize)
+            # simMMRe: bSize, prpNum, bSize
+            simMMRe = simMMRe.squeeze()
+            ftr_match_pair_list = list()
+            ftr_unmatch_pair_list = list()
+
+            for i in range(bSize):
+                for j in range(bSize):
+                    if i==j:
+                        ftr_match_pair_list.append(simMMRe[i, ..., i])
+                    elif lblList[i]!=lblList[j]:
+                        ftr_unmatch_pair_list.append(simMMRe[i, ..., j])
+
+            ftr_match_pair_mat = torch.stack(ftr_match_pair_list, 0)
+            ftr_unmatch_pair_mat = torch.stack(ftr_unmatch_pair_list, 0)
+            match_num = len(ftr_match_pair_list)
+            unmatch_num = len(ftr_unmatch_pair_list)
+            if match_num>0:
+                entro_loss = self.entropy_calculator(ftr_match_pair_mat)
+            loss +=self.lamda2*entro_loss 
+            print('entropy loss: %3f ' %(float(entro_loss)))
+            #pdb.set_trace()
+        print('\n')
+        return loss
+
     def forwardCoAtt(self, simMMRe, lblList):
         bSize = len(lblList)
         if(len(lblList)==1):
@@ -131,7 +324,7 @@ class lossEvaluator(nn.Module):
         loss = torch.zeros(1).cuda()
         pairNum = 0.000001
 
-#        pdb.set_trace()
+        #pdb.set_trace()
         t1 = time.time()
         #print(lblList)
         if not self.struct_only_flag:
@@ -325,10 +518,11 @@ class lossGroundR(nn.Module):
         return loss
 
 def build_lossEval(opts):
-    if opts.wsMode == 'rankTube' or opts.wsMode=='coAtt' or opts.wsMode=='coAttV2' or opts.wsMode=='coAttV3' or opts.wsMode == 'coAttV4' or  opts.wsMode=='rankFrm':
+    if opts.wsMode == 'rankTube' or opts.wsMode=='coAtt' or opts.wsMode=='coAttV2' or opts.wsMode=='coAttV3' or opts.wsMode == 'coAttV4' or  opts.wsMode=='rankFrm' or opts.wsMode=='coAttBi':
         loss_criterion = lossEvaluator(opts.margin, opts.biLoss, opts.lossW, \
                 opts.lamda, opts.struct_flag, opts.struct_only, \
-                opts.entropy_regu_flag, opts.lamda2)
+                opts.entropy_regu_flag, opts.lamda2, \
+                loss_type=opts.loss_type)
         loss_criterion.wsMode =opts.wsMode
         return loss_criterion
     elif opts.wsMode =='rankGroundR' or opts.wsMode =='coAttGroundR' or opts.wsMode=='rankGroundRV2':
